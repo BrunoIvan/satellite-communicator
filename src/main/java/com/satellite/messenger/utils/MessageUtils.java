e7e4dbb646e53b0a89d6be6de3685984dcf38476
BrunoIvan <bruno.ivan.otero@gmail.com>
2021-05-03 08:59:06 -0300

    Improved get-message method

--- src/main/java/com/satellite/messenger/utils/MessageUtils.java
+++ src/main/java/com/satellite/messenger/utils/MessageUtils.java
@@ -1,5 +1,6 @@
 package com.satellite.messenger.utils;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,25 +11,35 @@ import static java.util.stream.IntStream.range;
 
 public class MessageUtils {
 
-    private static void addIfPossible(Set<String> words, final List<String> aWords, final int i) {
-        if (aWords.size() > i) {
-            words.add(aWords.get(i));
-        }
-    }
-
     public static String getMessage(final List<String> aWords, final List<String> bWords, final List<String> cWords) {
         final List<List<String>> wordsSets = Stream.of(aWords, bWords, cWords)
-                .sorted((i, j) -> i.size() > j.size() ? -1 : 0)
+                .sorted((i, j) -> i.size() < j.size() ? -1 : 0)
                 .collect(Collectors.toList());
-        final Set<String> words = new LinkedHashSet<>();
+        final List<String> result = new ArrayList<>();
         range(0, wordsSets.get(0).size())
-                .forEach(i -> {
-                    addIfPossible(words, wordsSets.get(0), i);
-                    addIfPossible(words, wordsSets.get(1), i);
-                    addIfPossible(words, wordsSets.get(2), i);
+                .forEach(index -> {
+                    final Set<String> words = getWordsR(0, index, wordsSets, new LinkedHashSet<>());
+                    if (words.size() == 1) {
+                        result.addAll(words);
+                    } else {
+                        throw new IllegalArgumentException("Could not determine message");
+                    }
                 });
-        words.remove("");
-        return String.join(" ", words);
+        return String.join(" ", result);
+    }
+
+    private static Set<String> getWordsR(final int setIndex, final int wordIndex, final List<List<String>> wordsSets, final LinkedHashSet<String> result) {
+        if (wordsSets.size() == setIndex) {
+            return result;
+        }
+
+        final int difference = wordsSets.get(setIndex).size() - wordsSets.get(0).size();
+        final String word = wordsSets.get(setIndex).get(wordIndex + difference);
+        if (!word.equals("")) {
+            result.add(word);
+        }
+
+        return getWordsR(setIndex + 1, wordIndex, wordsSets, result);
     }
 
 }
